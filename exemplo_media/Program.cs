88130b759376216e8da8ca8a8ed20cded3a15996
MarcelaMendonca <112650728+MarcelaMendonca@users.noreply.github.com>
2023-04-05 15:33:42 -0300

    Add files via upload

---
+++ exemplo_media/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exemplo;
+class Program
+{
+    static void Main(string[] args)
+    
+     {
+        Aluno aluno1 = new Aluno();
+
+        Console.WriteLine("Digite seu nome: ");
+        string? nome = Console.ReadLine();
+        Console.WriteLine("Seu nome é: " + nome);
+        Console.Write("Digite sua média de matemática: ");
+        aluno1.nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite sua média de português: ");
+        aluno1.nota2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Você está: ");
+
+        
+        aluno1.mensagem();
+    }
+}
