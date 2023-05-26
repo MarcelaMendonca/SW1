a2ae79e9cf63fb11b8a0708942826ef2986b41d7
MarcelaMendonca <112650728+MarcelaMendonca@users.noreply.github.com>
2023-05-26 17:32:26 -0300

    Add files via upload

---
+++ Contratados/contratopessoafisica.cs
@@ -0,0 +1,11 @@
+namespace Contratados;
+class contratopessoafisica: contratado
+{
+    public string? CPF {get; set;}
+    public int Idade {get; set;}
+
+
+    public string MostrarDados(){
+        return " ";
+    }
+}
---
+++ Contratados/contratopessoajuridica.cs
@@ -0,0 +1,12 @@
+namespace Contratados;
+class contratopessoajuridica: contratado
+{
+    public string? CNPJ {get; set;}
+    public string? IE {get; set;}
+    public string? Empresa {get; set;}
+
+
+    public string MostrarDados(){
+        return " ";
+    }
+}
